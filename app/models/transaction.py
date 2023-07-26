20d6e7d4d1b49ce0c158078b8d89c169d937b75a
Jack Smith <smithjj7@cardiff.ac.uk>
2023-07-26 14:01:45 +0100

    separated user and transaction models

---
+++ app/models/transaction.py
@@ -0,0 +1,55 @@
+from .. import db
+from sqlalchemy import Column, ForeignKey, String, Integer, DateTime, PickleType
+from uuid import uuid4
+
+
+class Transaction(db.Model):
+    """Transaction model for storing transaction data."""
+    __tablename__ = 'transaction'
+    
+    id = Column(String(36), primary_key=True, nullable=False)
+    amount_pence = Column(Integer, nullable=False)
+    datetime = Column(DateTime, nullable=False)
+
+    merchant_id = Column(Integer, ForeignKey("merchant.id"))
+    merchant_mcc = Column(Integer, nullable=True)
+
+    receipt = db.relationship('Receipt', backref='receipt', lazy='dynamic', cascade='all, delete')
+
+    user_id = Column(String(36), ForeignKey("user.id"))
+    feed_log_id = Column(String(36), ForeignKey("feed_log.id"))
+
+    co2e = Column(Integer, nullable=True)
+    estimate = db.relationship('Estimate', backref='estimate', lazy='select')
+
+
+class Receipt(db.Model):
+    """Receipt model for storing receipt data."""
+    __tablename__ = 'receipt'
+
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid4()))
+    transaction_id = Column(String(36), ForeignKey("transaction.id"), nullable=False, unique=True)
+    items = Column(PickleType, nullable=False)
+    
+
+class Merchant(db.Model):
+    """Merchant model for storing merchant data."""
+    __tablename__ = 'merchant'
+
+    id = Column(String(36), primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    mcc = Column(Integer, nullable=False)
+
+    transaction_id = db.relationship('Transaction', backref='merchant', lazy='select')
+
+
+class FeedLog(db.Model):
+    """Feed Log model for tracking pull requests that return data."""
+    __tablename__ = 'feed_log'
+
+    id = Column(String(36), primary_key=True, nullable=False)
+    datetime = Column(DateTime, nullable=False, default=db.func.now())
+
+    transactions = db.relationship('Transaction', backref='feed_log', lazy='dynamic', cascade='all, delete')
+    user_id = Column(String(36), ForeignKey("user.id"))
+    item_count = Column(Integer, nullable=False)
--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,8 @@
 from .. import db, login_manager
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from sqlalchemy import Column, ForeignKey, String, Boolean, Integer, DateTime, PickleType
+from sqlalchemy import Column, String, Boolean, DateTime
 from uuid import uuid4
-
 
 
 class User(UserMixin, db.Model):
@@ -40,63 +39,6 @@ class User(UserMixin, db.Model):
     def __repr__(self) -> String:
         """Returns a string representation of the user in key-item pairs."""
         return '{id: ' + str(self.id) + ', name: ' + self.name + ', email: ' + self.email + '}'
-    
-
-
-class FeedLog(db.Model):
-    """Feed Log model for tracking pull requests that return data."""
-    __tablename__ = 'feed_log'
-
-    id = Column(String(36), primary_key=True, nullable=False)
-    datetime = Column(DateTime, nullable=False, default=db.func.now())
-
-    transactions = db.relationship('Transaction', backref='feed_log', lazy='dynamic', cascade='all, delete')
-    user_id = Column(String(36), ForeignKey("user.id"))
-    item_count = Column(Integer, nullable=False)
-
-
-
-class Transaction(db.Model):
-    """Transaction model for storing transaction data."""
-    __tablename__ = 'transaction'
-    
-    id = Column(String(36), primary_key=True, nullable=False)
-    amount_pence = Column(Integer, nullable=False)
-    datetime = Column(DateTime, nullable=False)
-
-    merchant_id = Column(Integer, ForeignKey("merchant.id"))
-    merchant_mcc = Column(Integer, nullable=True)
-
-    receipt = db.relationship('Receipt', backref='receipt', lazy='dynamic', cascade='all, delete')
-
-    user_id = Column(String(36), ForeignKey("user.id"))
-    feed_log_id = Column(String(36), ForeignKey("feed_log.id"))
-
-    co2e = Column(Integer, nullable=True)
-    estimate = db.relationship('Estimate', backref='estimate', lazy='select')
-
-
-
-class Receipt(db.Model):
-    """Receipt model for storing receipt data."""
-    __tablename__ = 'receipt'
-
-    id = Column(String(36), primary_key=True, default=lambda: str(uuid4()))
-    transaction_id = Column(String(36), ForeignKey("transaction.id"), nullable=False, unique=True)
-    items = Column(PickleType, nullable=False)
-    
-
-
-class Merchant(db.Model):
-    """Merchant model for storing merchant data."""
-    __tablename__ = 'merchant'
-
-    id = Column(String(36), primary_key=True, nullable=False)
-    name = Column(String, nullable=False)
-    mcc = Column(Integer, nullable=False)
-
-    transaction_id = db.relationship('Transaction', backref='merchant', lazy='select')
-
 
 
 @login_manager.user_loader
