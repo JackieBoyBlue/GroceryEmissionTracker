9649f1bdc2c525dd2b2698726565a44562a7c5a6
Jack Smith <smithjj7@cardiff.ac.uk>
2023-08-08 10:58:41 +0100

    changed get_category_from_vectors to take tuples (category_name, vector) so it can return the name of the best category

--- app/data_processing/models/_interface.py
+++ app/data_processing/models/_interface.py
@@ -38,19 +38,20 @@ class EmbeddingModelInterface(ABC):
         return (most_similar, cosines[most_similar])
     
     
-    def get_category_from_vectors(self, compare_this: list, to_this: list, *and_this: list, get_all_cosines: bool = False) -> tuple:
+    def get_category_from_vectors(self, compare_this: list, to_this: tuple, *and_this: tuple, get_all_cosines: bool = False) -> tuple:
         """Takes a target vector and n vectors to compare it to, and returns the most similar vector and its cosine similarity."""
 
         if not isinstance(compare_this, list): raise TypeError('compare_this must be a list')
-        if not isinstance(to_this, list): raise TypeError('to_this must be a list')
-        if not all(isinstance(arg, list) for arg in and_this): raise TypeError('and_this must be a list')
+        if not isinstance(to_this, tuple): raise TypeError('to_this must be a tuple with (category_name, vector)')
+        if not all(isinstance(arg, tuple) for arg in and_this): raise TypeError('and_this must be a tuple with (category_name, vector)')
 
         cosines = {}
 
         counter = 0
         compare_to = [to_this] + [arg for arg in and_this]
+
         for to_this in compare_to:
-            cosines[compare_to[counter]] = 1 - cosine(compare_this, to_this)
+            cosines[to_this[0]] = 1 - cosine(compare_this, to_this[1])
             counter += 1
 
         if get_all_cosines: return cosines
